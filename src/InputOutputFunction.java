610b781f84173315f7c157cb877ff719f2978561
Feris-Alexsy <ahmadlamborghiniavetador>
2022-09-04 18:55:00 +0700

    Initial commit

---
+++ src/InputOutputFunction.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InputOutputFunction {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Program penjumlahan sangat sederhana");
+        System.out.print("masukan angka pertama : ");
+        int value = scanner.nextInt();
+        System.out.print("Masukan Angka kedua : ");
+        int anotherValue = scanner.nextInt();
+        int result = value + anotherValue;
+        System.out.println("Hasilnya adalah : " + result);
+    }
+}
